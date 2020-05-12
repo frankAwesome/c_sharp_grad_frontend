3ea3c680bf55bc192f7f7f3d266c9c7d0b3fb0d4
Francois de Klerk <francoisdk@bbd.co.za>
2020-05-12 14:02:39 +0200

    added logout

---
+++ c_sharp_grad_frontend/wwwroot/js/sweethelper.js
@@ -0,0 +1,33 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your Javascript code.
+
+function alert() {
+    event.preventDefault();
+    Swal.fire({
+        title: 'Are you sure?',
+        text: "You will be logged out!",
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Yes, log out!'
+    }).then((result) => {
+        if (result.value) {
+            Swal.fire(
+                'Logout!',
+                'Successful logout.',
+                'success'
+            )
+            var millisecondsToWait = 2000;
+            setTimeout(function () {
+                // Whatever you want to do after the wait
+                window.location = "https://localhost:44338/";
+            }, millisecondsToWait);
+            
+        }
+    })
+}
+
+
