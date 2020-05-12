f6f2ecae01469c14a29dc4d0ef6f674b248c22f8
Francois de Klerk <francoisdk@bbd.co.za>
2020-05-12 13:02:53 +0200

    Register added

---
+++ c_sharp_grad_frontend/Helpers/RegisterHelper.cs
@@ -0,0 +1,40 @@
+﻿using c_sharp_grad_frontend.Models;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace c_sharp_grad_frontend.Helpers
+{
+    public class RegisterHelper
+    {
+
+        public RegisterHelper(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+        public IConfiguration Configuration { get; }
+
+        public async Task<bool> CallAuthService(User model)
+        {
+            var json = JsonConvert.SerializeObject(model);
+            var httpContent = new StringContent(json, Encoding.UTF8, "application/json");
+            var httpClient = new HttpClient(SSLHelper.GetSSL());
+            var response = await httpClient.PostAsync(Configuration.GetConnectionString("RegisterService"), httpContent);
+            //var responseString = await response.Content.ReadAsStringAsync();
+
+            if (response.StatusCode == HttpStatusCode.Created)
+                return true;
+            else
+                return false;
+        }
+    }
+
+
+    
+}
--- c_sharp_grad_frontend/Pages/Register.cshtml.cs
+++ c_sharp_grad_frontend/Pages/Register.cshtml.cs
@@ -2,16 +2,48 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using c_sharp_grad_frontend.Helpers;
+using c_sharp_grad_frontend.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Configuration;
 
 namespace c_sharp_grad_frontend.Pages
 {
     public class RegisterModel : PageModel
     {
+        public RegisterModel(IConfiguration configuration, IToken _token)
+        {
+            Configuration = configuration;
+            token = _token;
+        }
+        public IConfiguration Configuration { get; }
+        public IToken token { get; }
+
         public void OnGet()
         {
 
+        }
+
+        public async Task<IActionResult> OnPost()
+        {
+            //the Configuration allow us to get connection strings from the appsetting.json config file
+            try
+            {
+                var helper = new RegisterHelper(Configuration);
+                var model = new User();
+                model.username = Request.Form["username"];
+                model.password = Request.Form["password"];
+
+                if (await helper.CallAuthService(model))
+                    return RedirectToPage("/RegisterSuccess");
+                else
+                    return RedirectToPage("/AuthFailed");
+            }
+            catch (Exception)
+            {
+                return RedirectToPage("/AuthFailed");
+            }
         }
     }
 }
