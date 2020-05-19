027f2a37be33ad70db18e82d068a1ca90029f2d5
Francois de Klerk <francoisdk@bbd.co.za>
2020-05-19 18:27:31 +0200

    models and config update

--- c_sharp_grad_frontend/Models/Donations.cs
+++ c_sharp_grad_frontend/Models/Donations.cs
@@ -4,12 +4,15 @@ namespace c_sharp_grad_frontend.Models
 {
     public class Donations
     {
-        public int Id { get; set; }
-        public int UserId { get; set; }
+        public string Username { get; set; }
         public decimal Amount { get; set; }
         public int OrganizationId { get; set; }
         public DateTime TimeStamp { get; set; }
         public string Description { get; set; }
+
+        public string ExtraOne { get; set; }
+        public string ExtraTwo { get; set; }
+        public string ExtraThree { get; set; }
 
     }
 }
