6980370d55ab9e894828c0abcf3e1060ae5aba59
Francois de Klerk <francoisdk@bbd.co.za>
2020-05-25 08:17:14 +0200

    new helper

--- c_sharp_grad_frontend/Pages/AdminHome.cshtml.cs
+++ c_sharp_grad_frontend/Pages/AdminHome.cshtml.cs
@@ -2,15 +2,29 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using c_sharp_grad_frontend.Helpers;
+using c_sharp_grad_frontend.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Configuration;
 
 namespace c_sharp_grad_frontend.Pages
 {
     public class AdminHomeModel : PageModel
     {
-        public void OnGet()
+        IToken token;
+        IConfiguration configuration;
+        public AdminHomeModel(IToken _token, IConfiguration _configuration)
         {
+            token = _token;
+            configuration = _configuration;
+
+        }
+        public async Task OnGet()
+        {
+            var helper = new DonationHelper(configuration, token);
+            var allUserDonation = helper.GetAllDonations();
+
 
         }
     }
